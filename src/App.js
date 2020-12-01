bb9bba1b3a9192616ae2d03f72e2f81f49693b6a
Tyler Cookson <tcookson0805@gmail.com>
2020-11-30 18:27:47 -0600

    1) implemented both a class and a react element to mimic the blink element that makes content within visible and invisible in .25 second increments without shifting surrounding content...2) included additional features for blink compenent to handle child componenets, accept custome element tags, and increse or decrease the blink rate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import './App.css';
-import { Blink } from './components/Blink'; 
+import Blink from './components/Blink';
 import './style/Blink.css';
 
+class TestLink extends React.Component {
+  render() {
+    return (
+      <a href="https://www.leftfieldlabs.com/">Child Components</a>
+    )
+  }
+}
 
 function App() {
   return (
@@ -10,8 +18,22 @@ function App() {
         <h1>The Blink tag</h1>
       </header>
       <div className="content">
-        <span className="blink">Make this blink!</span>
-        <Blink>Or make this blink!</Blink>
+        <div className="features">
+          <h2>Basic Requirements</h2>
+          <ol>
+            <li>Contents within the tag are preset to alternate between being visibible/invisible in 1/4 second increments</li>
+            <li>Wheather using the <span className="blink">blink class</span> or using the custom <Blink>Blink</Blink> component, the surrounding text does not shift.</li>
+          </ol>
+        </div>
+
+        <div className="features">
+          <h2>The Blink component also has additional features that allow it to:</h2>
+          <ol>
+            <li>handle<Blink> <TestLink /> </Blink></li>
+            <li>accept <Blink tag="strong"> custom element </Blink>tags</li>
+            <li>and even <Blink rate=".1">increase</Blink> or <Blink rate="4">decrease</Blink> its blink rate</li>
+          </ol>
+        </div>
       </div>
     </div>
   );
--- src/components/Blink.jsx
+++ src/components/Blink.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+// import react-blink from './style/Blink.css'
 
-export const Blink = (props) => {
-    return(
-    <div className="react-blink">{props.children}</div>
-    )
+class Blink extends React.Component {
+	constructor(props) {
+		super(props);
+		this.content = this.props.content || '';
+		this.tag = this.props.tag || 'blink';
+		this.rate = this.props.rate || '1';
+		this.children = this.props.children || [];
+	}
+
+	render() {
+		const CustomTag = this.tag
+		const style = {
+			'animation': `blinkingText ${this.rate}s infinite`
+		}
+
+		return (
+			<CustomTag className="react-blink" style={style}>
+				{this.children}
+			</CustomTag>
+		);
+	}
 }
+
+export default Blink;
